4cea723887a9389b36a1b59597e9a44275567fe0
numbnet <zusyurec@gmail.com>
2021-01-06 02:58:53 +0200

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]
    
     Changes not staged for commit:
    	modified:   home/.oh-my-zsh (modified content)

---
+++ make/start.termux/komponents/DarkFly.sh
@@ -0,0 +1,34 @@
+Установка DarkFly (tools 500+)
+Установка DarkFly в termux (tools 500+)
+Множество разных инструментов:
+
+1)
+pkg install python2
+
+
+2)
+pkg install git
+
+
+3)
+pkg install php
+
+
+4)
+git clone https://github.com/Ranginang67/DarkFly-Tool
+
+
+5)
+cd DarkFly-Tool
+
+
+6)
+chmod +x install.py
+
+
+7)
+python2 install.py
+
+
+8) Меню установщика запускаем командой
+DarkFly
